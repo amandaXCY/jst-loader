a8cc00b7da7f7e87fe527d1f4fb0dd93e82a3240
Amanda <xuchengyun@beisen.com>
2017-10-24 09:48:23 +0800

    js-loader

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const loaderUtils = require("loader-utils");
-const template = require('lodash/template');
+const _ = require('lodash');
 
 module.exports = function(content, map){
-    const config = loaderUtils.getOptions(this);
+    var defaults = this.options({
+        templateSettings: {},
+        prettify:false
+      });
+  
+    const config = _.extend({},defaults,loaderUtils.getOptions(this);)
+    const callback = this.async();
+
+    
+    try {
+        const templateSettings = config.templateSettings || {};
+        let compiled = _.template(src, false, options.templateSettings).source;
+    } catch (err) {
+        
+        throw new Error(err)
+    }
+
+    if (config.prettify) {
+        compiled = compiled.replace(/\n/g, '');
+    }
 
     //对source进行解析
-    var callback = this.async();
 
     var exportsString = "export default = ";
     if (config.exportAsDefault) {
@@ -14,5 +32,5 @@ module.exports = function(content, map){
     } else if (config.exportAsMoudle) {
         exportsString = "module.exports = ";
     }
-    return exportsString + template(content)  + ";";
+    return exportsString + compiled + ";";
 }
